0734a93a5aa6af100a3844773c1a245e2bea303b
Kevin <kevin199932@gmail.com>
2024-02-21 00:08:03 +0800

    Changed file path

--- app/lib/get_data.js
+++ app/lib/get_data.js
@@ -2,7 +2,7 @@ import fsPromises from 'fs/promises'
 import path from 'path'
 
 export async function getLocalData() {
-    const filePath = path.join(process.cwd(), 'app\\lib\\output_ict.json');
+    const filePath = path.join(process.cwd(), 'app/lib/output_ict.json');
     const jsonData = await fsPromises.readFile(filePath);
     const data = JSON.parse(jsonData);
 

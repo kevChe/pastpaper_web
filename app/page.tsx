582528000ff51feffda4a8b0d7322ac65db91bc3
Kevin <kevin199932@gmail.com>
2024-02-21 23:14:42 +0800

    UI fixed

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   const question_details = await getLocalData()
   // console.log(questions[0].Question)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-200">
         <Questions />
     </main>
   );
--- app/ui/flipcard.tsx
+++ app/ui/flipcard.tsx
@@ -18,18 +18,20 @@ export default function Card(question_details: question_details ){
     const cardRef = useRef<HTMLDivElement>(null);
     const questionRef = useRef<HTMLDivElement>(null);
     const answerRef = useRef<HTMLDivElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
     useEffect(() => {
-        if (cardRef.current && questionRef.current && answerRef.current) {
+        if (cardRef.current && questionRef.current && answerRef.current && textareaRef.current) {
             const cardHeight = questionRef.current.scrollHeight;
-            cardRef.current.style.height = `${cardHeight}px`;
-            questionRef.current.style.height = `${cardHeight}px`;
-            answerRef.current.style.height = `${cardHeight}px`;
-            questionRef.current.style.justifyContent = "space-evenly";
-            answerRef.current.style.justifyContent = "space-evenly";
+            const textareaHeight = textareaRef.current.scrollHeight
+            cardRef.current.style.height = `${cardHeight }px`;
+            // questionRef.current.style.height = `${cardHeight + textareaHeight}px`;
+            // answerRef.current.style.height = `${cardHeight + textareaHeight}px`;
+            // questionRef.current.style.justifyContent = "space-between";
+            // answerRef.current.style.justifyContent = "space-between";
             console.log(answerRef.current.style)
         }
-    });
+    }, [cardRef, questionRef, answerRef]);
 
     const handleFlip = () => {
         setFlip(!flip)
@@ -37,7 +39,7 @@ export default function Card(question_details: question_details ){
     
     const FlipButton = () =>{
         return(
-            <button className="text-black border-black" onClick={handleFlip}>CLick</button>
+            <button className=" text-black border-black border-2 w-1/3 rounded-full shadow-md py-1" onClick={handleFlip}>CLick to Reveal Answer</button>
         )
     }
 
@@ -62,23 +64,25 @@ export default function Card(question_details: question_details ){
         //     </div>
 
         // </div>
-        <div ref={cardRef} className="group w-full [perspective:2000px] border-blue-600 border-2" style={{ minHeight: 'fit-content' }}>
-            <div className={clsx(
-                "relative w-full flex justify-between border-black border-2 shadow-xl rounded-xl transition-all duration-500 [transform-style:preserve-3d]",
+        <div  className="group w-full [perspective:2000px] border-black border-2 p-4 rounded-xl shadow-2xl bg-white" style={{ minHeight: 'fit-content' }}>
+            <div ref={cardRef} className={clsx(
+                "relative w-full flex justify-between border-black border-2 shadow-lg rounded-xl transition-all duration-350 [transform-style:preserve-3d] mb-6 bg-slate-50",
                 {
                     "[transform:rotateY(180deg)]": flip === true
                 }
             )}>
-                <div ref={questionRef} className="absolute h-fit flex flex-col inset-0 w-full [backface-visibility:hidden] p-4 border-red-500 border-2">
-                    <h1 className=" text-3xl text-black">{question}</h1>
-                    <input className="border-2 border-black "/>
-                    <FlipButton />
+                <div ref={questionRef} className="absolute inset-0 w-full [backface-visibility:hidden]" >
+                    <div className="p-3"><h1 className=" text-2xl text-black">{question}</h1></div>
+                    
                 </div>
-                <div ref={answerRef} className="absolute justify-evenly h-fit inset-0 p-4 w-full rounded-xl bg-black/80 border-red-500 border-2 text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-                    <h1 className="text-3xl font-bold text-white h-fit">{answer}</h1>
-                    <FlipButton />
+                <div ref={answerRef} className="absolute inset-0 w-full [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                    <div className="p-3"><h1 className="text-2xl text-black h-fit">{answer}</h1></div>
+                    
                 </div>
             </div>
+            <textarea ref={textareaRef} className="shadow-lg p-4 w-full border-2 border-black text-black  mb-6" rows={3}>  </textarea>
+            <div className="w-full flex justify-center"><FlipButton /></div>
+            
         </div>
     )
 }

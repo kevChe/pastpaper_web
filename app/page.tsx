bd3289883c8ca28efda49d3a1c8a21668ca57faf
Kevin <kevin199932@gmail.com>
2024-02-23 21:26:35 +0800

    Added suspense to useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,21 @@ import FlipCard from "./ui/flipcard";
 import Questions from "@/app/ui/questions"
 import {getLocalData} from '@/app/lib/get_data';
 import Search from "./ui/search";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 
 export default async function Home() {
   // console.log(questions[0].Question)
 
   const question_details = await getLocalData();
+  
   return (
     <main className="flex  min-h-screen flex-col items-center justify-between p-24 bg-slate-200 border-2 border-black">
         <Search />
-        <Questions question_details={question_details}/>
+        <Suspense fallback="<div>Loading...</div>">
+          <Questions question_details={question_details} />
+        </Suspense>
     </main>
   );
 }

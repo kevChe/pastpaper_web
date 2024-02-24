87b4b76015763353bd6a909b3d0e41fde54a1884
Kevin <kevin199932@gmail.com>
2024-02-24 12:23:37 +0800

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
   return (
     <main className="flex  min-h-screen flex-col items-center justify-between p-24 bg-slate-200 border-2 border-black">
         {/* <Search /> */}
-        <Suspense fallback="<div>Loading...</div>"> */}
+        <Suspense fallback="<div>Loading...</div>">
                  <Questions question_details={question_details} />
         </Suspense>
     </main>

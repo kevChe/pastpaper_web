9398ab8c8bef9a775eed4c6004b7b680b5ede643
Kevin <kevin199932@gmail.com>
2024-02-24 12:17:46 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ import { Suspense } from "react";
 export default async function Home() {
   // console.log(questions[0].Question)
 
-  const question_details = await getLocalData();
+  // const question_details = await getLocalData();
   
   return (
     <main className="flex  min-h-screen flex-col items-center justify-between p-24 bg-slate-200 border-2 border-black">
         <Search />
         <Suspense fallback="<div>Loading...</div>">
-                 <Questions question_details={question_details} />
+                 {/* <Questions question_details={question_details} /> */}
         </Suspense>
     </main>
   );

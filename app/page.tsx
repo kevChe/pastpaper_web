465dc3e64440b1a8722ee6c505cf3e33deeab146
unknown <kevin199932@gmail.com>
2024-02-27 08:54:58 +0800

    Added pac man

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Questions from "@/app/ui/questions"
 import {getLocalData} from '@/app/lib/get_data';
 import Search from "./ui/search";
 import { Suspense } from "react";
+import { PacmanLoader } from "react-spinners";
 
 
 export default async function Page() {
@@ -11,10 +12,10 @@ export default async function Page() {
   
   return (
     <main className="flex  min-h-screen flex-col items-center justify-between p-10 md:p-12 bg-slate-200 border-2 border-black">
-        <Suspense fallback="<div>Loading...</div>">
+        <Suspense fallback= {<PacmanLoader/>}>
           <Search />
         </Suspense>
-        <Suspense fallback="<div>Loading...</div>">
+        <Suspense fallback="">
                  <Questions question_details={question_details} />
         </Suspense>
     </main>
